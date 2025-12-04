3830a120c87e43668a94837f9211929fbc49dc55
Gokulnath Selvaraj <selvara.gokulnath@divum.in>
2025-12-04 18:03:51 +0530

    Updated the service account code.

--- server/notification/firebase.js
+++ server/notification/firebase.js
@@ -1,6 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-account.json"); 
+let serviceAccount;
 
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require("./service-account.json");
+}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
