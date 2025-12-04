6f54ef2abc4abb0005adbbb68caecb3440be1b4f
Gokulnath Selvaraj <selvara.gokulnath@divum.in>
2025-12-04 20:28:52 +0530

    Updated the schema for user.js.

--- server/models/User.js
+++ server/models/User.js
@@ -28,6 +28,10 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    fcmToken: {
+      type: String,
+      default: null,
+    },
     status: {
       type: String,
       enum: ["online", "offline"],
